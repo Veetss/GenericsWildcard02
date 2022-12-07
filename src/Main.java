2e36566521a4be4bc6134108dbfd9227316a73bf
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:44:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GenericClass<Integer> genericClass1= new GenericClass();
+        GenericClass<Integer> genericClass2 = new GenericClass();
+        GenericClass<String> genericClass3 = new GenericClass();
+        GenericClass<String> genericClass4 = new GenericClass();
+
+        genericClass1.setT(12);
+        genericClass2.setT(13);
+        genericClass3.setT("This is a string");
+        genericClass4.setT("This is a string");
+
+        System.out.println(GenericClass.isEqual(genericClass1, genericClass2));
+        System.out.println(GenericClass.isEqual(genericClass3, genericClass4));
     }
 }
